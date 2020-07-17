930ee4304624494a4db1d7c039ea720a5685f48f
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-07-18 00:16:20 +0200

    created working script to check postgres version:

---
+++ src/main/java/com/codecool/JavaPostgreSqlVersion.java
@@ -0,0 +1,35 @@
+package com.codecool;
+
+//package com.zetcode;
+import java.sql.*;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JavaPostgreSqlVersion {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+
+        String url = "jdbc:postgresql://localhost:5432/application_process_db";
+        String user = "postgres";
+        String password = "sMuGa1@1";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT VERSION()")) {
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(JavaPostgreSqlVersion.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+}
